d2b42db66b7db1ff0d9c1cdb315ef79ff1a1095b
fectec <fectec151@gmail.com>
2025-05-23 09:45:39 -0600

    fix: apply .gitignore and clean up repository

--- ros2_ws/build/uf_ros_lib/prefix_override/sitecustomize.py
+++
@@ -1,4 +0,0 @@
-import sys
-if sys.prefix == '/usr':
-    sys.real_prefix = sys.prefix
-    sys.prefix = sys.exec_prefix = '/home/fectec/X-ARM_VisualServoing/ros2_ws/install/uf_ros_lib'
--- ros2_ws/build/xarm_sdk/CMakeFiles/xarm_cxx_sdk.dir/cxx/src/serial/impl/win.cc.o.d
+++
@@ -1,3 +0,0 @@
-CMakeFiles/xarm_cxx_sdk.dir/cxx/src/serial/impl/win.cc.o: \
- /home/fectec/X-ARM_VisualServoing/ros2_ws/src/xarm_ros2/xarm_sdk/cxx/src/serial/impl/win.cc \
- /usr/include/stdc-predef.h
